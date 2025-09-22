03e5d4a1a664e1a4ea843257537406d5aff2fe97
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-22 12:18:17 +0530

    aading arguments

---
+++ 12-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hi $PERSON2 how are you"
+echo "$PERSON2: hi $PERSON1 i am good how are you"
+echo "$PERSON1: i am also good what are you learning now"
+echo "$PERSON2: i am learnig shell scrpting"
