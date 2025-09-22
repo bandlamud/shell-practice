ce6c9e7de662de353d824a1344c95c2bff8274ef
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-22 20:59:50 +0530

    applying conditions

--- 15-condition.sh
+++ 15-condition.sh
@@ -4,6 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER  -lt 10 ]; then
     echo "Giving number $NUMBER is less then 10"
+elif [ $NUMBER -eq 10 ]; then
+    echo "Giving number $NUMBER is equle to 10"
 else
     echo "Giving number $NUMBER is greater then 10"
 fi
