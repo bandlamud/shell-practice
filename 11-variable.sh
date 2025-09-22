b8ae0c4b2f0265df5df1018dc8dc686f71b2758d
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-22 12:12:34 +0530

    repacing names

---
+++ 11-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1="balu"
+PERSON2="bharath"
+
+echo "$PERSON1: hi $PERSON2 how are you"
+echo "$PERSON2: hi $PERSON1 i am good how are you"
+echo "$PERSON1: i am also good what are you learning now"
+echo "$PERSON2: i am learnig shell scrpting"
