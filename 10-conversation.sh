8f241498a57b235c718f74bf8433831366476a66
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-22 12:08:29 +0530

    conversation

---
+++ 10-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: hi suresh how are you"
+echo "suresh: hi ramesh i am good how are you"
+echo "ramesh: i am also good what are you learning now"
+echo "suresh: i am learnig shell scrpting"
